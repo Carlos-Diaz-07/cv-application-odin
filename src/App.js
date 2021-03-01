091e41a0db0104b0912e6f432d6e702bebc48bde
Ty-Mejia <diazmelian.carlos@gmail.com>
2021-03-01 13:11:44 +0000

    app module adjusted for new modules

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
 import React, { Component } from "react";
-import Overview from "./components/Overview";
+import TasksOverview from "./components/SkillsOverview";
+import Jobs from "./components/Jobs";
 import PersonalInfo from "./components/PersonalInfo";
-
+import Education from "./components/Education";
+import Skills from "./components/Skills";
 export class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			task: "",
-			tasks: [],
-		};
-	}
+	// constructor() {
+	// 	super();
+	// 	this.state = {
+	// 		task: "",
+	// 		tasks: [],
+	// 	};
+	// }
 
-  handleChange = (e) => {
-    this.setState({
-      task: e.target.value
-    })
-  }
+	// handleChange = (e) => {
+	// 	this.setState({
+	// 		task: e.target.value,
+	// 	});
+	// };
 
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task:'',
-    })
-  }
+	// onSubmitTask = (e) => {
+	// 	e.preventDefault();
+	// 	this.setState({
+	// 		tasks: this.state.tasks.concat(this.state.task),
+	// 		task: "",
+	// 	});
+	// };
 
 	render() {
-		const { task, tasks } = this.state;
-
 		return (
 			<div>
-				<form onSubmit={this.onSubmitTask} >
-          <label htmlFor='taskInput'>Enter Task</label>
-          <input onChange={this.handleChange} value={task} type='text' id='taskInput' />
-          <button type='submit'>Add Task</button>
-        </form>
-        <div>
-          <Overview tasks={tasks}/>
-        </div>
-        <div>
-          <PersonalInfo />
-        </div>
+				<div>
+					<PersonalInfo />
+				</div>
+				<div>
+					<Jobs />
+				</div>
+				<div>
+					<Education />
+				</div>
+				<div>
+					<Skills />
+				</div>
 			</div>
 		);
 	}

f526996b53e821b98730a0a09966d9aaa4b6234a
Ty-Mejia <diazmelian.carlos@gmail.com>
2021-03-01 11:56:18 +0000

    Jobs modules basic functionality created

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,16 +1,137 @@
-import React from "react";
-import uniqid from 'uniqid';
+import JobsOverview from "./JobsOverview";
+import React, { Component } from "react";
 
-function Overview(props) {
-	const { tasks } = props;
+class Jobs extends Component {
+	constructor() {
+		super();
+		this.state = {
+			companyName: "",
+			city: "",
+			fromDate: "",
+			toDate: "",
+			role: "",
+			description: "",
+			allJobsInfo: [],
+		};
+	}
 
-	return (
-		<ul>
-			{tasks.map((task) => {
-				return <li id={uniqid()}>{task}</li>;
-			})}
-		</ul>
-	);
+	handleChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	onSubmitJob = (e) => {
+		e.preventDefault();
+		const {
+			companyName,
+			city,
+			fromDate,
+			toDate,
+			role,
+			description,
+		} = this.state;
+
+		this.setState({
+			allJobsInfo: this.state.allJobsInfo.concat(
+				companyName,
+				city,
+				fromDate,
+				toDate,
+				role,
+				description
+			),
+			companyName: "",
+			city: "",
+			fromDate: "",
+			toDate: "",
+			role: "",
+			description: "",
+		});
+	};
+
+	render() {
+		const {
+			companyName,
+			city,
+			fromDate,
+			toDate,
+			role,
+			description,
+			allJobsInfo,
+		} = this.state;
+
+		return (
+			<div>
+				<div>
+					<JobsOverview allJobs={allJobsInfo} />
+				</div>
+				<form onSubmit={this.onSubmitJob}>
+					<div>
+						<label htmlFor="companyNameInput">Company Name</label>
+						<input
+							onChange={this.handleChange}
+							name="companyName"
+							value={companyName}
+							type="text"
+							id="companyNameInput"
+						/>
+					</div>
+					<div>
+						<label htmlFor="cityInput">City</label>
+						<input
+							onChange={this.handleChange}
+							name="city"
+							value={city}
+							type="text"
+							id="cityInput"
+						/>
+					</div>
+					<div>
+						<label htmlFor="fromDateInput">From</label>
+						<input
+							onChange={this.handleChange}
+							name="fromDate"
+							value={fromDate}
+							type="date"
+							id="fromDateInput"
+						/>
+					</div>
+					<div>
+						<label htmlFor="toDateInput">To</label>
+						<input
+							onChange={this.handleChange}
+							name="toDate"
+							value={toDate}
+							type="date"
+							id="toDateInput"
+						/>
+					</div>
+					<div>
+						<label htmlFor="roleInput">Role</label>
+						<input
+							onChange={this.handleChange}
+							name="role"
+							value={role}
+							type="text"
+							id="roleInput"
+						/>
+					</div>
+					<div>
+						<label htmlFor="descriptionInput">Further Description</label>
+						<textarea
+							onChange={this.handleChange}
+							name="description"
+							value={description}
+							type="text"
+							id="descriptionInput"
+						>
+							Anything to Add?
+						</textarea>
+					</div>
+					<button type="submit">+Add</button>
+				</form>
+			</div>
+		);
+	}
 }
 
-export default Overview;
+export default Jobs;
---
+++ src/components/JobsOverview.js
@@ -0,0 +1,16 @@
+import React from "react";
+import uniqid from 'uniqid';
+
+function JobsOverview(props) {
+	const { allJobs } = props;
+
+	return (
+		<div id={uniqid()}>
+			{allJobs.map((job) => {
+				return <li id={uniqid()}>{job}</li>;
+			})}
+		</div>
+	);
+}
+
+export default JobsOverview;
